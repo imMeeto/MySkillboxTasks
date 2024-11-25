993e3f7a710213af3dc359b30e8a0fca4dc9af07
imMeeto <meeto.collab@gmail.com>
2024-11-25 22:58:02 +0300

    save

--- SKILLBOX TASKS/Page_4.2_Task_4_inprogress/Page_4.2_Task_4.cpp
+++ SKILLBOX TASKS/Page_4.2_Task_4_inprogress/Page_4.2_Task_4.cpp
@@ -1,3 +1,25 @@
-int main() {
+#include <iostream>
 
+int main() {
+  int people;
+  int barbers;
+  std::cout << "---Рассчет необходимого количества барберов.---" << "\n";
+  std::cout << "Введите количество человек проживающих в городе: " ;
+  std::cin >> people;
+  int men = people / 2;
+  std::cout << "По статистике, в городе проживает 50% мужчин, поэтому мужчин: " << men << "\n";
+
+  std::cout << "Введите количество уже работающих барберов:  " ;
+  std::cin >> barbers;
+
+  int servedByBarber = 8;
+
+  float barbersPerMonth = men / 8;
+  float needBarbers = barbersPerMonth - barbers;
+  float barbersPerDay;
+  barbersPerDay = needBarbers / 30;
+
+  std::cout << "Так как в месяц требуется подстричь " << men << " мужчин, каждый день дожно работать " << barbersPerDay << " барберов." << "\n";
+  float necessaryToHire = needBarbers / 30 - barbers;
+  std::cout << "Так как у вас уже работает " << barbers << " барберов, необходимо нанять " << necessaryToHire << " барберов." << "\n";
 }
