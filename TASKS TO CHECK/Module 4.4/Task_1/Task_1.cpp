4361df62926da5506ebb52ee143fc1e64d7b0624
imMeeto <meeto.collab@gmail.com>
2024-12-01 13:28:32 +0300

    save

--- TASKS TO CHECK/Module 4.4/Task_1/Task_1.cpp
+++ TASKS TO CHECK/Module 4.4/Task_1/Task_1.cpp
@@ -4,11 +4,18 @@ int main() {
   int firstNumber;
   int secondNumber;
   std::cout << "Ищем минимальное значение из двух чисел." << "\n";
+<<<<<<< HEAD
   std::cout << "Введите первое число: ";
   std::cin  >> firstNumber;
   std::cout << "Введите второе число: ";
   std::cin  >> secondNumber;
   std::cout << "---Проверяем---" << "\n";
+=======
+  std::cout << "Введите первое число: " << "\n";
+  std::cin  >> firstNumber;
+  std::cout << "Введите второе число: " << "\n";
+  std::cin  >> secondNumber;
+>>>>>>> 93d30b5 (save)
   
   if(firstNumber > secondNumber) {
     std::cout << "Наименьшее число: " << secondNumber << "\n";
