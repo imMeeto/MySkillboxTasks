a320e625df21e2731689a3435246c2f121086efe
imMeeto <meeto.collab@gmail.com>
2024-12-08 10:41:32 +0300

    save

--- TASKS TO CHECK/Module 5.6/Task_1.cpp
+++ TASKS TO CHECK/Module 5.6/Task_1.cpp
@@ -2,25 +2,25 @@
 
 int main() 
 {
-std::cout <<  "Проверьте, правильным ли эшелоном летит самолёт." << "\n";
+  std::cout <<  "Проверьте, правильным ли эшелоном летит самолёт." << "\n";
 
-int flightLevel;
-std::cout << "Введите эшелон полета в метрах: ";
-std::cin >> flightLevel;
+  int flightLevel;
+  std::cout << "Введите эшелон полета в метрах: ";
+  std::cin >> flightLevel;
 
-int speed;
-std::cout << "Введите скорость самолета в км/ч: ";
-std::cin >> speed;
+  int speed;
+  std::cout << "Введите скорость самолета в км/ч: ";
+  std::cin >> speed;
 
-std::cout << "------------------------------" << "\n";
+  std::cout << "------------------------------" << "\n";
 
-if (flightLevel < 9000 || flightLevel > 9500 && speed < 750 || speed > 850)
-{
-  std::cout << "Это не верный эшелон полета." << "\n";
-} else { 
-  std::cout << "Это верный эшелон полета." << "\n";
-}
+    if (flightLevel < 9000 || flightLevel > 9500 && speed < 750 || speed > 850)
+    {
+      std::cout << "Это не верный эшелон полета." << "\n";
+    } else { 
+      std::cout << "Это верный эшелон полета." << "\n";
+    }
 
-return 0;
+  return 0;
 }
 
