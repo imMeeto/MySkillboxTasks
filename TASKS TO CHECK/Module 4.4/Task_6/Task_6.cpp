fcc711ca027697fb7a2d5bc2c98de480d797a514
imMeeto <meeto.collab@gmail.com>
2024-12-13 15:24:32 +0300

    save

--- TASKS TO CHECK/Module 4.4/Task_6/Task_6.cpp
+++ TASKS TO CHECK/Module 4.4/Task_6/Task_6.cpp
@@ -1,33 +1,16 @@
 #include <iostream>
 
 int main() {
-  int people;
   int barbersCount;
+  int mensCount;
   std::cout << "---Рассчет необходимого количества барберов.---" << "\n";
 
-    while (true) {
-      std::cout << "Введите количество человек проживающих в городе: ";
-      std::cin >> people;
-        if (people < 1000) {
-          std::cout << "В городе не может проживать так мало людей! Введите значение от 1000!" << "\n";
-        } else {
-          break;
-        } 
-    }
+  std::cout << "Введите количество мужчин проживающих в городе: ";
+  std::cin >> mensCount;
 
-  int mensCount = people / 2;
-  std::cout << "По статистике, в городе проживает 50% мужчин, поэтому мужчин: " << mensCount << "\n";
+  std::cout << "Введите количество уже работающих барберов: ";
+  std::cin >> barbersCount;
 
-    while (true) {
-      std::cout << "Введите количество уже работающих барберов: ";
-      std::cin >> barbersCount;
-        if (barbersCount < 0) {
-          std::cout << "Вы ввели неверное значение! Если у вас нет барберов, то введите 0." << "\n";
-        } else {
-          break;
-        } 
-    }
-  
   std::cout << "Производим рассчет..." << "\n";
   std::cout << "------------------------------------------------" << "\n";
 
@@ -35,17 +18,13 @@ int main() {
 
   // Сколько человек успеет постричь барбер за месяц?
   int mansPerBarberPerMonth = mansPerBarber * 30;
-  std::cout << "Один барбер стрижет столько клиентов в месяц " << mansPerBarberPerMonth << "\n";
+  std::cout << "Один барбер стрижет столько клиентов в месяц: " << mansPerBarberPerMonth << "\n";
 
   // Сколько нужно барберов чтобы постричь mensCount человек в месяц?
   int requiredBarbersCount = mensCount / mansPerBarberPerMonth;
-  if (requiredBarbersCount * mansPerBarberPerMonth < mensCount) {
-  requiredBarbersCount += 1;
-  }
 
-  if (mensCount % mansPerBarberPerMonth != 0)
-  {
-    requiredBarbersCount + 1;
+  if (mensCount % mansPerBarberPerMonth != 0) {
+    requiredBarbersCount += 1;
   }
 
   std::cout << "Необходимое число барберов: " << requiredBarbersCount << "\n";
