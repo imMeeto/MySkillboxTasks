195a402f9d2ee8b04682846aec174e6de043a2be
Meeto <meeto.collab@gmail.com>
2025-01-30 20:12:22 +0300

    save

--- TASKS TO CHECK/Module 8.6/Task_3/Task_3.cpp
+++ TASKS TO CHECK/Module 8.6/Task_3/Task_3.cpp
@@ -4,7 +4,7 @@ int main() {
   std::cout << "Производство игровых деревянных кубиков 5x5x5 см." << "\n";
 
   float X, Y, Z;
-  std::cout << "Введите размеры бруска: ";
+  std::cout << "Введите размеры бруска: " << "\n";
   std::cout << "X: ";
   std::cin >> X;
   std::cout << "Y: ";
@@ -16,11 +16,18 @@ int main() {
   float woodenBar = X * Y * Z;
   float makedCubes = woodenBar / CUBE;
 
-  std::cout << "Из этого бруска можно изготовить: " << makedCubes << " кубиков.";
+  std::cout << "Из этого бруска можно изготовить: " << makedCubes << " кубиков." << "\n"; //должно быть 49 потому что не хватает на ещё 1.355 кубик
+
+  int set = 2;
+  for (int i = 0; i < set; i++) {
+    set = set*set*set;
+    if (set > makedCubes) {
+      break;
+    }
+    
+  }
 
 
-
-  std::cout << "Из них можно составить набор из " << // code << " кубиков.";
-
+  std::cout << "Из них можно составить набор из " << set << " кубиков."; //должен получиться набор из 27 кубиков
 
 }
