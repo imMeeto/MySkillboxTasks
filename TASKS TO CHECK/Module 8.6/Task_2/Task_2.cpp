dab091b0c95ff2b79d9588e24859895ad847517e
Meeto <meeto.collab@gmail.com>
2025-01-30 18:37:58 +0300

    save

---
+++ TASKS TO CHECK/Module 8.6/Task_2/Task_2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+  std::cout << "[Игра]: Убей орка огненным шаром!" << "\n";
+
+  float health, magicResistance, damage = 0;
+
+  std::cout << "Введите количество здоровья орка ";
+  std::cin >> health;
+  std::cout << "Введите количество сопротивления магии: ";
+  std::cin >> magicResistance;
+  std::cout << "Введите количество урона: ";
+  std::cin >> damage;
+  std::cout << "[Игра]: Стреляем по орку огненным шаром!" << "\n";
+
+  
+
+}
