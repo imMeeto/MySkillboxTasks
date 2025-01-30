598da2e25d03032acb3c71c3e1cfa04e005a0673
Meeto <meeto.collab@gmail.com>
2025-01-30 19:58:26 +0300

    save

--- TASKS TO CHECK/Module 8.6/Task_3/Task_3.cpp
+++ TASKS TO CHECK/Module 8.6/Task_3/Task_3.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
 
 int main() {
-  
+  std::cout << "Производство игровых деревянных кубиков 5x5x5 см." << "\n";
+
+  float X, Y, Z;
+  std::cout << "Введите размеры бруска: ";
+  std::cout << "X: ";
+  std::cin >> X;
+  std::cout << "Y: ";
+  std::cin >> Y;
+  std::cout << "Z: ";
+  std::cin >> Z;
+
+  const int CUBE = 125; // 5*5*5
+  float woodenBar = X * Y * Z;
+  float makedCubes = woodenBar / CUBE;
+
+  std::cout << "Из этого бруска можно изготовить: " << makedCubes << " кубиков.";
+
+
+
+  std::cout << "Из них можно составить набор из " << // code << " кубиков.";
+
+
 }
