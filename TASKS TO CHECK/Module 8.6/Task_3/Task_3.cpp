2d88dbe4c348314441480089252914f3d5e6daeb
Meeto <meeto.collab@gmail.com>
2025-01-31 15:40:31 +0300

    save

--- TASKS TO CHECK/Module 8.6/Task_3/Task_3.cpp
+++ TASKS TO CHECK/Module 8.6/Task_3/Task_3.cpp
@@ -1,44 +1,35 @@
 #include <iostream>
-#include <cmath> // для функции std::floor
+#include <cmath>
 
 int main() {
     std::cout << "Производство игровых деревянных кубиков 5x5x5 см." << "\n";
 
-    const int CUBE_X = 5, CUBE_Y = 5, CUBE_Z = 5;
-    const int CUBE_FULL = CUBE_X * CUBE_Y * CUBE_Z; // основной кубик 5*5*5 см
-
-    float woodBarX, woodBarY, woodBarZ;
-    std::cout << "Введите размеры бруска: " << "\n";
+    float X, Y, Z;
+    std::cout << "Введите размеры бруска (X, Y, Z):\n";
     std::cout << "X: ";
-    std::cin >> woodBarX;
+    std::cin >> X;
     std::cout << "Y: ";
-    std::cin >> woodBarY;
+    std::cin >> Y;
     std::cout << "Z: ";
-    std::cin >> woodBarZ;
+    std::cin >> Z;
 
-    // Проверка на минимальный размер бруска
-    if (woodBarX < 5 || woodBarY < 5 || woodBarZ < 5) {
+    if (X < 5 || Y < 5 || Z < 5) {
         std::cout << "Размеры бруска должны быть не менее 5 см." << "\n";
         return 1;
     }
 
-    float woodBarFull = woodBarX * woodBarY * woodBarZ;
-    int makedCubes = static_cast<int>(woodBarFull / CUBE_FULL); // округление вниз до целого числа
+    // Отбрасываем дробную часть. Делим размеры бруска на 5 и округляем вниз, чтобы получить количество кубиков по каждому размеру
+    int cubes = static_cast<int>(X / 5) * static_cast<int>(Y / 5) * static_cast<int>(Z / 5);
+    std::cout << "Из этого бруска можно изготовить " << cubes << " кубиков." << "\n";
 
-    std::cout << "Из этого бруска можно изготовить: " << makedCubes << " кубиков." << "\n";
-
-    // Проверка на то, сколько готовых кубиков уместится в набор (box), минимальный размер набора — 2x2x2 = 8 кубиков.
-    int box = 2;
-    int willBoxes = 0;
-
-    while (willBoxes <= makedCubes) {
-        willBoxes = box * box * box;
-        box++;
+    // Вычисляем кубический корень из общего количества кубиков
+    int maxSet = std::cbrt(cubes);
+    // Проверяем, можем ли мы собрать набор
+    if (maxSet * maxSet * maxSet <= cubes) {
+        std::cout << "Из них можно составить набор из " << maxSet * maxSet * maxSet << " кубиков." << "\n";
+    } else {
+        std::cout << "Набор собрать нельзя." << "\n";
     }
-
-    // Вывод количества кубиков в наборе
-    int cubesInSet = (box - 2) * (box - 2) * (box - 2); // -2 для учета одного кубика
-    std::cout << "Из них можно составить набор из " << cubesInSet << " кубиков." << "\n";
 
     return 0;
 }
