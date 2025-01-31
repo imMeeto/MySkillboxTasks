70e535bed34d93b941199df13c91f7562ca00a42
Meeto <meeto.collab@gmail.com>
2025-01-31 18:40:38 +0300

    save

--- TASKS TO CHECK/Module 8.6/Task_5/Task_5.cpp
+++ TASKS TO CHECK/Module 8.6/Task_5/Task_5.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
+#include <cmath>
 
 int main() {
-  
+  std::cout << "Расчёта среднего темпа бега.\n";
+
+  float kilometers = 0.0f, seconds = 0.0f;
+  int minutes = 0;
+  int pace = 0;
+
+  std::cout << "Привет, Сэм! Сколько километров ты сегодня пробежал? ";
+  std::cin >> kilometers;
+
+int allPace;
+  for (int kmCount = 1; kilometers >= kmCount; kmCount++) {
+    std::cout << "Какой у тебя был темп на километре " << kmCount << "? ";
+    std::cin >> pace;
+    allPace += pace;
+  }
+
+  int arithmeticMean = (allPace / kilometers);
+
+  minutes = arithmeticMean / 60;
+  seconds = round(arithmeticMean % 60);
+
+  std::cout << "Твой средний темп за тренировку: " << minutes << " минут " << seconds << " секунд.\n";
+
+  return 0;
 }
