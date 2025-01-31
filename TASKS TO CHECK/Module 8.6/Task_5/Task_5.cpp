11e482f52538a1af439e344847138a3eb792f35a
Meeto <meeto.collab@gmail.com>
2025-01-31 18:58:59 +0300

    save

--- TASKS TO CHECK/Module 8.6/Task_5/Task_5.cpp
+++ TASKS TO CHECK/Module 8.6/Task_5/Task_5.cpp
@@ -2,26 +2,39 @@
 #include <cmath>
 
 int main() {
-  std::cout << "Расчёта среднего темпа бега.\n";
+  std::cout << "Расчёт среднего темпа бега.\n";
 
   float kilometers = 0.0f, seconds = 0.0f;
   int minutes = 0;
   int pace = 0;
+  int allPace = 0; // Инициализация переменной
 
   std::cout << "Привет, Сэм! Сколько километров ты сегодня пробежал? ";
   std::cin >> kilometers;
 
-int allPace;
-  for (int kmCount = 1; kilometers >= kmCount; kmCount++) {
+  // Проверка ввода на количество километров
+  if (kilometers <= 0) {
+    std::cout << "Количество километров должно быть положительным числом.\n";
+    return 1;
+  }
+
+  for (int kmCount = 1; kmCount <= kilometers; kmCount++) {
     std::cout << "Какой у тебя был темп на километре " << kmCount << "? ";
     std::cin >> pace;
+
+    // Проверка ввода на темп
+    if (pace < 0) {
+      std::cout << "Темп не может быть отрицательным.\n";
+      return 1;
+    }
+
     allPace += pace;
   }
 
-  int arithmeticMean = (allPace / kilometers);
+  float arithmeticMean = allPace / kilometers;
 
-  minutes = arithmeticMean / 60;
-  seconds = round(arithmeticMean % 60);
+  minutes = (arithmeticMean) / 60;
+  seconds = static_cast<int>(arithmeticMean) % 60; 
 
   std::cout << "Твой средний темп за тренировку: " << minutes << " минут " << seconds << " секунд.\n";
 
