1bbb0427166418515bc163a003d1d2bd42554326
Meeto <meeto.collab@gmail.com>
2025-02-02 20:36:43 +0300

    save

--- TASKS TO CHECK/Module 9.7/Task_2/Task_2.cpp
+++ TASKS TO CHECK/Module 9.7/Task_2/Task_2.cpp
@@ -1,6 +1,51 @@
 #include <iostream>
 
 int main() {
+    std::cout << "Check if the float number is written correctly.\n";
 
-return 0;
+    std::string input;
+    std::cout << "Input float number: ";
+    std::cin >> input;
+
+    if (input.empty()) {
+        std::cout << "No!\n";
+        return 0;
+    }
+
+    int dotCount = 0; // Счетчик точек
+    bool hasDigit = false; // Флаг наличия цифры
+
+    // Проверка первого символа
+    if (input[0] == '-' || input[0] == '.' || (input[0] >= '0' && input[0] <= '9')) {
+        if (input[0] >= '0' && input[0] <= '9') {
+            hasDigit = true;
+        }
+    } else {
+        std::cout << "No!\n";
+        return 0;
+    }
+
+    // Проверка остальных символов
+    for (int i = 1; i < input.length(); ++i) {
+        if (input[i] >= '0' && input[i] <= '9') {
+            hasDigit = true; // Найдена цифра
+        } else if (input[i] == '.') {
+            dotCount++; // Если нашли точку, то увеличиваем счетчик
+            if (dotCount > 1) {
+                std::cout << "No!\n";
+                return 0;
+            }
+        } else {
+            std::cout << "No!\n";
+            return 0;
+        }
+    }
+
+    if (hasDigit) {
+        std::cout << "Yes!\n";
+    } else {
+        std::cout << "No!\n";
+    }
+
+    return 0;
 }
