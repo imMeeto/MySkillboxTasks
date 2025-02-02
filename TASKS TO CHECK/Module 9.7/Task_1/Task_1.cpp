b1a11b01c8810aeea1b8f519e212678aeb9fa55e
Meeto <meeto.collab@gmail.com>
2025-02-02 16:08:50 +0300

    save

--- TASKS TO CHECK/Module 9.7/Task_1/Task_1.cpp
+++ TASKS TO CHECK/Module 9.7/Task_1/Task_1.cpp
@@ -8,27 +8,24 @@ int main() {
   int totalDepMinutes = 0;
   int totalArrMinutes = 0;
 
-  std::cout << "Input departure time (HH:MM): ";
-  std::cin >> depTime;
-
   while (true) {
     std::cout << "Input departure time (HH:MM): ";
     std::cin >> depTime;
 
-    if (depTime.length() !=5 || depTime[2] != ':' 
+    if (depTime.length() != 5 || depTime[2] != ':' 
         || depTime[0] < '0' || depTime[0] > '9'
         || depTime[1] < '0' || depTime[1] > '9'
         || depTime[3] < '0' || depTime[3] > '9'
         || depTime[4] < '0' || depTime[4] > '9') {
-      std::cout << "Error! Input (HH:MM) format!\n";
-      continue;;
+      std::cout << "Error! Input correct format!\n";
+      continue;
     } else {
       int depHours = (depTime[0] - '0') * 10 + (depTime[1] - '0');
       int depMinutes = (depTime[3] - '0') * 10 + (depTime[4] - '0');
 
       if (depHours > 23 || depMinutes > 59) {
-        std::cout << "Error! Input (HH:MM) format!\n";
-        continue;;
+        std::cout << "Error! Input correct format!\n";
+        continue;
       } else {
         totalDepMinutes = 60 * depHours + depMinutes; // Get all departure time in minutes
         break;
@@ -37,23 +34,23 @@ int main() {
   }  
 
   while (true) {
-    std::cout << "Input departure time (HH:MM): ";
+    std::cout << "Input arrival time (HH:MM): ";
     std::cin >> arrTime;
 
-    if (arrTime.length() !=5 || arrTime[2] != ':' 
+    if (arrTime.length() != 5 || arrTime[2] != ':' 
         || arrTime[0] < '0' || arrTime[0] > '9'
         || arrTime[1] < '0' || arrTime[1] > '9'
         || arrTime[3] < '0' || arrTime[3] > '9'
         || arrTime[4] < '0' || arrTime[4] > '9') {
-      std::cout << "Error! Input (HH:MM) format!\n";
-      continue;;
+      std::cout << "Error! Input correct format!\n";
+      continue;
     } else {
       int arrHours = (arrTime[0] - '0') * 10 + (arrTime[1] - '0');
       int arrMinutes = (arrTime[3] - '0') * 10 + (arrTime[4] - '0');
 
       if (arrHours > 23 || arrMinutes > 59) {
-        std::cout << "Error! Input (HH:MM) format!\n";
-        continue;;
+        std::cout << "Error! Input correct format!\n";
+        continue;
       } else {
         totalArrMinutes = 60 * arrHours + arrMinutes; // Get all arrival time in minutes
         break;
