0bbcb491d3543e713950b87969fa5d5578830cae
Meeto <meeto.collab@gmail.com>
2025-02-03 14:19:53 +0300

    save

--- TASKS TO CHECK/Module 9.7/Task_4/Task_4.cpp
+++ TASKS TO CHECK/Module 9.7/Task_4/Task_4.cpp
@@ -5,11 +5,24 @@ int main() {
 
   std::string firstNumber, secondNumber;
 
-  std::cout << "Input the desired number: ";
-
   while (true) {
-    if (firstNumber.length() != 4 || secondNumber.length() != 4) {
-      std::cout << "Number is too long. Input 4 digits!\n";
+    std::cout << "Input the desired number (4 digits): ";
+    std::cin >> firstNumber;
+
+    if (firstNumber.length() != 4) {
+      std::cout << "Error! Input 4 digits.\n";
+      continue;
+    } else {
+      break;
+    }
+  }
+
+    while (true) {
+    std::cout << "Input the second number (4 digits): ";
+    std::cin >> secondNumber;
+
+    if (secondNumber.length() != 4) {
+      std::cout << "Error! Input 4 digits.\n";
       continue;
     } else {
       break;
@@ -18,6 +31,28 @@ int main() {
 
   int bullsCount = 0;
   int cowsCount = 0;
+
+  // Подсчёт быков
+  for (int i = 0; i < 4; ++i) {
+      if (firstNumber[i] == secondNumber[i]) {
+          ++bullsCount;
+      }
+  }
+
+  // Подсчёт коров
+  for (int i = 0; i < 4; ++i) {
+      for (int j = 0; j < 4; ++j) {
+          if (i != j && firstNumber[i] == secondNumber[j]) {
+              ++cowsCount;
+              break;
+          }
+      }
+  }
+
+  // Корректировка коров, так как одна цифра может быть учтена несколько раз
+  cowsCount = std::min(cowsCount, 4 - bullsCount);
   
+  std::cout << "Bulls: " << bullsCount << ", cows: " << cowsCount << "\n";
+
   return 0;
 }
