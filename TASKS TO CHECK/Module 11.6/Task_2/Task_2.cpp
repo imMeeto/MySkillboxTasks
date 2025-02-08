fa3abeb9e9f7a52f5c3f2d69b8ac14ef715909d2
Meeto <meeto.collab@gmail.com>
2025-02-08 17:12:23 +0300

    save

---
+++ TASKS TO CHECK/Module 11.6/Task_2/Task_2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+char wordbook[] = {'!', '#', '$', '%', '&', '\'', '*', '+', '-',
+    '/', '=', '?', '^', '_', '`', '{', '|', '}', '~'};
+
+std::string leftPart(std::string email) {
+
+}
+
+std::string rightPart(std::string email) {
+
+}
+
+int main() {
+  std::cout << "*Make sure that the email address is correct.*\n";
+
+  std::string emailMain;
+
+  std::cout << "Enter your email address: ";
+  std::getline(std::cin, emailMain);
+  
+  return 0;
+}
