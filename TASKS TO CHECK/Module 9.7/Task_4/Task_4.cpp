5c42f1ae90a2efabff1ea70108e2614109fbb172
Meeto <meeto.collab@gmail.com>
2025-02-13 17:00:16 +0300

    save

--- TASKS TO CHECK/Module 9.7/Task_4/Task_4.cpp
+++ TASKS TO CHECK/Module 9.7/Task_4/Task_4.cpp
@@ -1,56 +1,55 @@
 #include <iostream>
+#include <string>
 
 int main() {
-  std::cout << "*Bulls and cows game.*\n";
-
-  std::string firstNumber, secondNumber;
-
-  while (true) {
-    std::cout << "[Player 1]: Input the desired number (4 digits): ";
-    std::cin >> firstNumber;
-
-    if (firstNumber.length() != 4) {
-      std::cout << "[Error]: Input 4 digits.\n";
-      continue;
-    } else {
-      break;
-    }
-  }
-
+    std::cout << "Bulls and cows game. \n";
+    std::string firstNumber, secondNumber;
+    
     while (true) {
-    std::cout << "[Player 2]: Input the second number (4 digits): ";
-    std::cin >> secondNumber;
-
-    if (secondNumber.length() != 4) {
-      std::cout << "[Error]: Input 4 digits.\n";
-      continue;
-    } else {
-      break;
+        std::cout << "[Player 1]: Input the desired number (4 digits): ";
+        std::cin >> firstNumber;
+        if (firstNumber.length() != 4) {
+            std::cout << "[Error]: Input 4 digits.\n";
+            continue;
+        } else {
+            break;
+        }
     }
-  }
+    
+    while (true) {
+        std::cout << "[Player 2]: Input the second number (4 digits): ";
+        std::cin >> secondNumber;
+        if (secondNumber.length() != 4) {
+            std::cout << "[Error]: Input 4 digits.\n";
+            continue;
+        } else {
+            break;
+        }
+    }
+    
+    int bulls = 0;
+    int cows = 0;
 
-  int bullsCount = 0;
-  int cowsCount = 0;
+    for (int i = 0; i < 4; ++i) {
+        if (firstNumber[i] == secondNumber[i]) {
+            ++bulls;
+            firstNumber[i] = '-';
+            secondNumber[i] = '-';
+        }
+    }
 
-  for (int i = 0; i < 4; ++i) {
-      if (firstNumber[i] == secondNumber[i]) {
-          ++bullsCount;
-      }
-  }
+    for (int i = 0; i < 4; ++i) {
+        if (firstNumber[i] != '-') {
+            for (int j = 0; j < 4; ++j) {
+                if (secondNumber[j] != '-' && firstNumber[i] == secondNumber[j]) {
+                    ++cows;
+                    secondNumber[j] = '-';
+                    break;
+                }
+            }
+        }
+    }
 
-  for (int i = 0; i < 4; ++i) { // Для каждой цифры i от 0 до 3 в загаданном числе
-      for (int j = 0; j < 4; ++j) { // проверяем каждую цифру j от 0 до 3 в названном числе
-          if (i != j && firstNumber[i] == secondNumber[j]) {
-              ++cowsCount;
-              break;
-          }
-      }
-  }
-
-  // Корректировка коров, так как одна цифра может быть учтена несколько раз
-  cowsCount = std::min(cowsCount, 4 - bullsCount);
-  
-  std::cout << "[Game]: Bulls: " << bullsCount << ", cows: " << cowsCount << "\n";
-
-  return 0;
+    std::cout << "[Game]: Bulls: " << bulls << ", cows: " << cows << "\n";
+    return 0;
 }
