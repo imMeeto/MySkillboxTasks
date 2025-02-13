c13e173028b8594f9ab1d58e4567b08c3f7960be
Meeto <meeto.collab@gmail.com>
2025-02-13 19:16:34 +0300

    save

--- TASKS TO CHECK/Module 12.5/Task_1/Task_1.cpp
+++ TASKS TO CHECK/Module 12.5/Task_1/Task_1.cpp
@@ -10,32 +10,28 @@ int main() {
   for (int i = 0; i < 10; i++) {
       std::cout << "- last name " << i + 1 << ": ";
       std::cin >> lastNames[i];
-    } 
+  } 
 
-    int apartmentsNum[3];
-    std::cout << "Input 3 apartment numbers:\n";
-      for (int i = 0; i < 3; i++) {
-        while (true) {
-        std::cout << "- apartment " << i + 1 << ": ";
-        std::cin >> apartmentsNum[i];
+  int apartmentsNum[3];
+  std::cout << "Input 3 apartment numbers:\n";
+  for (int i = 0; i < 3; i++) {
+    while (true) {
+      std::cout << "- apartment " << i + 1 << ": ";
+      std::cin >> apartmentsNum[i];
 
-        if (apartmentsNum[i] >= 1 && apartmentsNum[i] <= 10) {
-          break;
-        } else {
-          std::cout << "Invalid input. Please input a number between 1 and 10.\n";
-        }
-      }
-    }
-    
-    std::cout << "Result: \n";
-    for (int i = 0; i < 3; i++) {
-      int apartmentNum = apartmentsNum[i];
-      if (apartmentNum >= 1 || apartmentNum <= 10) {
-        std::cout << "In apartement " << apartmentNum << " live: " << lastNames[apartmentNum - 1] << "\n";
+      if (apartmentsNum[i] >= 1 && apartmentsNum[i] <= 10) {
+        break;
       } else {
-        std::cout << "Invalid apartment number " << apartmentNum <<  "!" << "\n";
+        std::cout << "Invalid input. Please input a number between 1 and 10.\n";
       }
     }
+  }
     
+  std::cout << "Result: \n";
+  for (int i = 0; i < 3; i++) {
+    int apartmentNum = apartmentsNum[i];
+    std::cout << "In apartement " << apartmentNum << " live: " << lastNames[apartmentNum - 1] << "\n";
+  }
+
     return 0;
 }
