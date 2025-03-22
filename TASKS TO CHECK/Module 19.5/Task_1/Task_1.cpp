7129d16bab94b96cdff532c0d4f724d0af7a055e
Your Name <meeto.collab@gmail.com>
2025-03-22 21:03:10 +0300

    save

--- TASKS TO CHECK/Module 19.5/Task_1/Task_1.cpp
+++ TASKS TO CHECK/Module 19.5/Task_1/Task_1.cpp
@@ -1,16 +1,36 @@
-#include <fstream>
-#include <vector>
 #include <iostream>
+#include <fstream>
+#include <string>
 
-int maint()
+int main()
 {
-  std::vector<char> vec;
-  std::ifstream Words;
-  Words.open("C:\\VS Code\\VS Code Projects\\MySkillboxTasks\\TASKS TO CHECK\\Module 19.5\\Task_1\\Words.txt");
-  
-  for (int i = 0; i < vec.size(); ++i)
-  {
-    std::cout << vec[i] << "\n";
-  }
+    std::ifstream Words;
+    Words.open("C:\\VS Code\\VS Code Projects\\MySkillboxTasks\\TASKS TO CHECK\\Module 19.5\\Task_1\\Words.txt");
 
+    if (!Words.is_open())
+    {
+      std::cout << "Error: file 'words.txt' not found!" << "\n";
+        return 1;
+    }
+
+    std::string targetWord;
+    std::cout << "Input the search word: ";
+    std::cin >> targetWord;
+
+    int count = 0;
+    std::string word;
+
+    while (Words >> word)
+    {
+        if (word == targetWord)
+        {
+            count++;
+        }
+    }
+
+    Words.close();
+
+    std::cout << "Word '" << targetWord << "' occurs " << count << " times.\n";
+
+    return 0;
 }
